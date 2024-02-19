9fd8f3d99fda794fe35bfe4d4425e87bfe662290
shaovoon <shaovoon@yahoo.com>
2024-02-19 13:22:01 +0800

    Remove the commented out code

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpRefReturnBenchmark
 {
@@ -27,54 +24,11 @@ namespace CSharpRefReturnBenchmark
     }
     class Program
     {
-        
-        /*
-        static void Main(string[] args)
-        {
-            // ref-local variable example
-            //int a = 10;
-            //ref int b = ref a;
-            //b = 20;
-            //Console.WriteLine("{0}", a); // display 20
-
-            RefReturnExample();
-            //CopyPropertyExample();
-            //PublicMemberExample();
-            Console.ReadKey();
-        }
-        
-        static void PublicMemberExample()
-        {
-            Coordinate cd = new Coordinate();
-            cd._Point.x = 10;
-            cd._Point.y = 20;
-            Console.WriteLine("{0},{1}", cd.Pt.x, cd.Pt.y); // display 10,20
-        }
-        static void CopyPropertyExample()
-        {
-            Coordinate cd = new Coordinate();
-            Point pt = cd.Pt; // a copy
-            pt.x = 10;
-            pt.y = 20;
-            Console.WriteLine("{0},{1}", cd.Pt.x, cd.Pt.y); // display 0,0
-            cd.Pt = pt;
-            Console.WriteLine("{0},{1}", cd.Pt.x, cd.Pt.y); // display 10,20
-
-        }
-        static void RefReturnExample()
-        {
-            Coordinate cd = new Coordinate();
-            ref Point pt = ref cd.RefPt;
-            pt.x = 10;
-            pt.y = 20;
-            Console.WriteLine("{0},{1}", cd.Pt.x, cd.Pt.y); // display 10,20
-        }
-        */
         static void DisplayElapseTime(string title, TimeSpan ts)
         {
             // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}.{2:000}",
-                ts.Minutes, ts.Seconds,
+            string elapsedTime = String.Format("{0:00}.{1:000}",
+                ts.Seconds,
                 ts.Milliseconds);
             Console.WriteLine(title + elapsedTime);
         }
@@ -93,8 +47,8 @@ namespace CSharpRefReturnBenchmark
             for(int i=0; i<list.Count; ++i)
             {
                 Point pt = list[i].Pt;
-                pt.x = 10;
-                pt.y = 20;
+                pt.x = i;
+                pt.y = i;
                 list[i].Pt = pt;
             }
 
@@ -107,8 +61,8 @@ namespace CSharpRefReturnBenchmark
             for (int i = 0; i < list.Count; ++i)
             {
                 ref Point pt = ref list[i].RefPt;
-                pt.x = 10;
-                pt.y = 20;
+                pt.x = i;
+                pt.y = i;
             }
 
             stopWatch2.Stop();
@@ -119,13 +73,14 @@ namespace CSharpRefReturnBenchmark
 
             for(int i=0; i<list.Count; ++i)
             {
-                list[i]._Point.x = 10;
-                list[i]._Point.y = 20;
+                list[i]._Point.x = i;
+                list[i]._Point.y = i;
             }
 
             stopWatch3.Stop();
             DisplayElapseTime("Public Member Access RunTime:", stopWatch3.Elapsed);
 
+            Console.WriteLine("Press any key and Enter to end the program!");
             Console.ReadKey();
         }
     }
